135abd55b0d319a5fa2e316f3e647462df7a6401
Sean Padden <sean.padden7@gmail.com>
2019-05-03 20:19:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(attendees)
   names_and_rooms = []
   index = [0, -1]
-  attendees.each do |name, index|
+  name.each do |name, index|
   	"Hello, #{name}! You'll be assigned to room #{index}!"
   end
 end
