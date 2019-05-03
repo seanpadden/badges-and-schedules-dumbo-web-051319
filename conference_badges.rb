1eaa412cb01712f5d9b23e6f43dab4fe2cc5d753
Sean Padden <sean.padden7@gmail.com>
2019-05-03 20:42:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  .collect do |name|
-    puts batch_badge_creator(attendees)
-  assign_rooms(attendees)
-end
+  batch_badge_creator(attendees).collect do |badges|
+    
+  
+  end
 end
