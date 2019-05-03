14c41d7c57c94dddfc96fba5d43f86675c9f5599
Sean Padden <sean.padden7@gmail.com>
2019-05-03 20:19:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  names_and_rooms = []
-  index = [0, -1]
-  name.each do |name, index|
-  	"Hello, #{name}! You'll be assigned to room #{index}!"
+  array_of_names.each do |name, index|
+  	"My name is #{name}. I am in position #{index} in the array"
+
   end
 end
